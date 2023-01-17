02ac7eacc37cb445f0e714d969c3aa9afb43de8d
Mauricio <mauricio_morales_angel@hotmail.com>
2023-01-17 00:48:41 -0300

    De a poco...

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -7,7 +7,7 @@ function CartItem({product}) {
     return  <li key={product.id} className="cartListItem">
                 <div className="cartListItemHeader">
                     <h3 className="cartListItemHeaderTitle">{product.name}</h3>
-                    <button className="cartListIItemHeaderBtn" onClick={() => removeItem(product.id)}>X</button>
+                    <button className="cartListIItemHeaderRemove" onClick={() => removeItem(product.id)}>X</button>
                 </div>
                 <figure className="cartListItemFigure">
                     <img src={product.image} alt={product.description} />
